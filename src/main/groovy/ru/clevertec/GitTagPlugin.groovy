000f564fe4c04b9594228a656af018ef80863bfb
Alex <fenix_al@tut.by>
2024-08-18 23:48:16 +0300

    setup: added basic structure for Groovy plugin

---
+++ src/main/groovy/ru/clevertec/GitTagPlugin.groovy
@@ -0,0 +1,13 @@
+package ru.clevertec
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+class GitTagPlugin implements Plugin<Project>{
+    @java.lang.Override
+    void apply(Project target) {
+
+    }
+}
+
